54c728ff86f0dddb25384f3dff1dce40c08140ea
gamni <gamnidhal91@gmail.com>
2024-05-31 17:56:28 +0200

    first commit

---
+++ src/main/java/com/technodev/capita/CapitaApplication.java
@@ -0,0 +1,13 @@
+package com.technodev.capita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technodev/capita/CapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technodev.capita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
